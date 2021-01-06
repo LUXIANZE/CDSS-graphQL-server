ff734bfeef640a2f3184f64f97e5e0b6d5c66b41
LUXIANZE <luxianze98@gmail.com>
2021-01-07 03:45:09 +0800

    Deployment setup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-import { ApolloServer } from 'apollo-server-express'
-import express from 'express'
-import typeDefs from './typeDefs'
-import resolvers from './resolvers'
-import { APP_PORT, IN_PROD, CONNECTION_STRING } from './config'
-import mongoose from 'mongoose'
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
+import { APP_PORT, IN_PROD, CONNECTION_STRING } from "./config";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  playground: !IN_PROD
+  playground: !IN_PROD,
   // mocks: true
-})
+});
 
-server.applyMiddleware({ app })
+server.applyMiddleware({ app });
 
-mongoose.set('useFindAndModify', false)
-mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true }).then(() => {
-  return app.listen({ port: APP_PORT })
-}).then((res) => {
-  console.log(`🚀 Server ready at http://localhost:${APP_PORT}${server.graphqlPath}`)
-})
+const port = process.env.PORT || APP_PORT;
+mongoose.set("useFindAndModify", false);
+mongoose
+  .connect(CONNECTION_STRING, { useNewUrlParser: true })
+  .then(() => {
+    return app.listen({ port: port });
+  })
+  .then((res) => {
+    console.log(
+      `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+    );
+  });
